88af5dba0e00ee5b341648935c8e77781842ec34
truyenho95 <truyenho.webdev@gmail.com>
2021-10-02 21:34:28 +0700

    change the body html

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Justin!'));
+app.get('/', (req, res) => res.send(
+    'Hello Justin đẹp trai!'
+));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
